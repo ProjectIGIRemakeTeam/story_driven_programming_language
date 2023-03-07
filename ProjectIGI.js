1d6a9990fb50ae7010078f09ae09c6c04c4d2b41
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-07 16:02:04 +0000

    11

--- ProjectIGI.js
+++ ProjectIGI.js
@@ -6,14 +6,18 @@
         <desc>A First Person Shooter about two espionage agents. Co-op function up to 2 players, other online game modes.
         Protagonist David Jones and his analyst Anya learn that a Russian Arms dealer named Josef Priboi had been kidnapped by Russian Mafia and imprisoned in a Military Airbase. After rescue Josef admits that his uncle Jach is selling a nuclear warhead to someone. After capturing the uncle, Jach Priboi admits to selling it to a Russian leader named EKK. Jones gets orders to transport Jach to headquarters with a chopper .But the chopper gets shot down by Migs near the Russian Border . EKK captures Jach and orders Jones to be killed, but Jones escapes and crosses the border and is rescued by a chopper. He hijacks the train carrying Jach, but EKK derails the train. Jones and Jach escape by a chopper sent by Anya. Jones gets orders to retrieve the warhead and terminate EKK. But he discovers a broken warhead (used to make a nuclear bomb) and fails to kill EKK as she escapes. Then he infiltrates the nuclear base where EKK planted the bomb. Jones kills EKK ,and Anya defuses the bomb. 
         </desc>
-        <plot_menu main="true">Start menu with new game, select map, settings
-        </plot_menu>
-        <plot_menu ingame="true">Character is shown in Third Person View when the game is paused.
-        </plot_menu>
+        <plot_menu main="true">Start menu with new game, select map, settings</plot_menu>
+        <plot_menu ingame="true">Character is shown in Third Person View when the game is paused.</plot_menu>
         <plot_element enemy="true">Enemy helicopter brings soldiers on alarm</plot_element>
         <plot_element playable="true">Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.</plot_element>
         <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
         <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
+        <plot_character name="David Llewellyn Jones" occupation="I.G.I. agent, former British Special Air Service operator" role="Protagonist" />
+        <plot_character name="Anya" occupation="Contact and mission director at headquarters" role="Mission director" />
+        <plot_character name="Jach Priboi" occupation="Soviet arms dealer" role="Antagonist" />
+        <plot_character name="Josef Priboi" occupation="Jach's nephew" role="Minor character" />
+        <plot_character name="Ekk" occupation="Homicidal Russian woman" role="Antagonist" />
+        <plot_character name="Captain Harrison" occupation="Commander of allied troops, ex-Green Beret in the US Army Special Forces" role="Secondary character" />
     </plot>
     <player_input>
         <input_device>Playstation 4 Controller</input_device>
