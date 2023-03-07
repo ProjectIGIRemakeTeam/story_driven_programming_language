addce4091d3ae6e79d320402ad8c7b1119b1fda8
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-07 14:58:38 +0000

    7
    plot_story tagging

--- Plot_Story.c++
+++ Plot_Story.c++
@@ -1,48 +1,37 @@
-class LevelTrainyard {
+class level_Trainyard {
 public:
-    // Some variables to store information about the level
-    int width = 800;
-    int height = 800;
-    string name = "Trainyard";
-    string mission = "secure a shipment of weapons before the enemy does and then extract safely.";
-        
-    void loadLevel() {
+    int width = 800; #trainyard,#width, #level, #plot_story
+    int height = 800; #trainyard,#height, #level, #plot_story
+    string name = "Trainyard"; #trainyard,#name
+    string mission = "secure a shipment of weapons before the enemy does and then extract safely."; #trainyard,#mission
+
+    void load_level() {
         // Load assets and objects specific to this level
     }
-        
-    void activateMission() {
+
+    void activate_mission() {
         // Set up the mission objectives, enemies, weapons, etc.
         cout << "Mission objective: " << mission << endl;
     }
 };
 
 
-class LevelAirbase {
+class level_Airbase {
 public:
-    // Some variables to store information about the level
-    int width = 1000;
-    int height = 1000;
-    string name = "Airbase";
-    string mission = "infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.";
-        
-    void loadLevel() {
+    int width = 1000; #airbase,#width, #level, #plot_story
+    int height = 1000; #airbase,#height, #level, #plot_story
+    string name = "Airbase"; #airbase,#name
+    string mission = "infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation."; #airbase,#mission
+
+    void load_level() {
         // Load assets and objects specific to this level
     }
-        
-    void activateMission() {
+
+    void activate_mission() {
         // Set up the mission objectives, enemies, weapons, etc.
         cout << "Mission objective: " << mission << endl;
     }
 };
 
-// Create instances of the levels
-LevelTrainyard trainyard;
-LevelAirbase airbase;
-
-// Now we can access the mission descriptions for each level like this:
-cout << "Trainyard mission: " << trainyard.mission << endl;
-cout << "Airbase mission: " << airbase.mission << endl;
-
-// And activate the mission objectives like this:
-trainyard.activateMission();
-airbase.activateMission();
+<plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
+<plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
--- gpt3.5/gpt3.5.js
+++ gpt3.5/gpt3.5.js
@@ -435,3 +435,56 @@ With this code, we can keep track of the mission objectives for each level and a
 
 
 
+You
+
+please add very detailed tagging on the elements. <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story> class LevelAirbase { public: // Some variables to store information about the level int width = 1000; #airbase,#airbase_width, #width, #level, #plot_story int height = 1000; #airbase,#airbase_height, #height, #level, #plot_story string name = "Airbase"; #airbase,#airbase_name string mission = "infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation."; void loadLevel() { // Load assets and objects specific to this level } void activateMission() { // Set up the mission objectives, enemies, weapons, etc. cout << "Mission objective: " << mission << endl; } }; etc
+ChatGPT
+Great job! The tags are very detailed and help to identify each element's purpose clearly. Keep up the good work!
+
+You
+
+send me back the file with the added tags please
+ChatGPT
+Here is the updated code with tags:
+
+Copy
+Insert
+New
+class level_Trainyard {
+public:
+    int width = 800; #trainyard,#width, #level, #plot_story
+    int height = 800; #trainyard,#height, #level, #plot_story
+    string name = "Trainyard"; #trainyard,#name
+    string mission = "secure a shipment of weapons before the enemy does and then extract safely."; #trainyard,#mission
+
+    void load_level() {
+        // Load assets and objects specific to this level
+    }
+
+    void activate_mission() {
+        // Set up the mission objectives, enemies, weapons, etc.
+        cout << "Mission objective: " << mission << endl;
+    }
+};
+
+
+class level_Airbase {
+public:
+    int width = 1000; #airbase,#width, #level, #plot_story
+    int height = 1000; #airbase,#height, #level, #plot_story
+    string name = "Airbase"; #airbase,#name
+    string mission = "infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation."; #airbase,#mission
+
+    void load_level() {
+        // Load assets and objects specific to this level
+    }
+
+    void activate_mission() {
+        // Set up the mission objectives, enemies, weapons, etc.
+        cout << "Mission objective: " << mission << endl;
+    }
+};
+
+<plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
+<plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
+Ask a question...
