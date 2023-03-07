6bebb4fc66dd8bbaae68159bff0c695134f875fd
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-07 14:12:34 +0000

    3

--- Story.js
+++ Story.js
@@ -1,18 +1,20 @@
 <story>
     <engine src="unreal5" platform="macos" format="c++">
-        {/* This part describes the video game engine and the platform used to write the video game */}
+        <!-- This part describes the video game engine and the platform used to write the video game -->
     </engine>
-    <plot type="FPS" number_of_playable_heros="2" offline_type="coop" online_type="all">
-        <desc>A First Person Shooter about two espionage agent. Co-op function up to 2 players, other online game modes.</desc>
-        <plot_element playable="true"> Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.<plot_element>
+    <plot type="FPS" number_of_playable_heroes="2" offline_type="coop" online_type="all">
+        <desc>A First Person Shooter about two espionage agents. Co-op function up to 2 players, other online game modes.</desc>
+        <plot_element playable="true">Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.</plot_element>
+        <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
+        <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
     </plot>
     <player_input>
-        <player_input>Playstation 4 Controller</player_input>
-        <player_input>Playstation 5 Controller</player_input>
-        <player_input>Xbox 360 Controller</player_input>
-        <player_input>Xbox One Controller</player_input>
-        <player_input>Xbox X Controller</player_input>
-        <player_input>Joy-Con</player_input>
-        <player_input>Keyboard and Mouse</player_input>
+        <input_device>Playstation 4 Controller</input_device>
+        <input_device>Playstation 5 Controller</input_device>
+        <input_device>Xbox 360 Controller</input_device>
+        <input_device>Xbox One Controller</input_device>
+        <input_device>Xbox X Controller</input_device>
+        <input_device>Joy-Con</input_device>
+        <input_device>Keyboard and Mouse</input_device>
     </player_input>
 </story>
--- gpt3.5/gpt3.5.js
+++ gpt3.5/gpt3.5.js
@@ -71,3 +71,42 @@ By adding NPCs, developers can provide a more compelling experience by making th
 Conclusion
 Incorporating these elements into the language can help game developers create games quickly and efficiently with a story-driven approach. Remember, we're working towards coding via storytelling techniques. These additions make the process simpler and can speed up development without sacrificing creativity or depth in the game.
 
+
+
+Second we can add plot_story, which describe a situation within the story. For example, Our hero(the one the player controls) have a mission on an airbase. (this will describe, who does, when, what, where) -> this is the level, where the plot of the story happens. <story> <engine src="unreal5" platform="macos" format="c++"> {/* This part describes the video game engine and the platform used to write the video game */} </engine> <plot type="FPS" number_of_playable_heros="2" offline_type="coop" online_type="all"> <desc>A First Person Shooter about two espionage agent. Co-op function up to 2 players, other online game modes.</desc> <plot_element playable="true"> Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.<plot_element> <plot_story>Our hero have a mission on an airbase. </plot_story> <plot_story>Our hero have a mission on a trainyard. </plot_story> </plot> <player_input> <player_input>Playstation 4 Controller</player_input> <player_input>Playstation 5 Controller</player_input> <player_input>Xbox 360 Controller</player_input> <player_input>Xbox One Controller</player_input> <player_input>Xbox X Controller</player_input> <player_input>Joy-Con</player_input> <player_input>Keyboard and Mouse</player_input> </player_input> </story>
+ChatGPT
+That's a great addition to the story! Adding plot_story elements can provide more context and specific details to the game's narrative, which can help the players immerse in the game world better. Here's an updated version of the story with the plot element:
+
+Copy
+Insert
+New
+<story>
+    <engine src="unreal5" platform="macos" format="c++">
+        <!-- This part describes the video game engine and the platform used to write the video game -->
+    </engine>
+    <plot type="FPS" number_of_playable_heroes="2" offline_type="coop" online_type="all">
+        <desc>A First Person Shooter about two espionage agents. Co-op function up to 2 players, other online game modes.</desc>
+        <plot_element playable="true">Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.</plot_element>
+        <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
+        <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
+    </plot>
+    <player_input>
+        <input_device>Playstation 4 Controller</input_device>
+        <input_device>Playstation 5 Controller</input_device>
+        <input_device>Xbox 360 Controller</input_device>
+        <input_device>Xbox One Controller</input_device>
+        <input_device>Xbox X Controller</input_device>
+        <input_device>Joy-Con</input_device>
+        <input_device>Keyboard and Mouse</input_device>
+    </player_input>
+</story>
+I also made some minor improvements to the story by fixing some typos and inconsistencies. For example, I changed "number_of_playable_heros" to "number_of_playable_heroes" for consistency and clarity, and I changed "" to "" for more accurate labeling.
+
+Alright. Creating a plot_story always has to describe the map/location/stage/level. From this every level will has an equalivent c++ code
+ChatGPT
+Yes, that is correct. Each plot_story will correspond to a specific level in the game, and these levels will have their own C++ code generated based on the story elements described within the plot_story.
+
+For example, an "airbase" level could be described with details such as layout of the base, objectives to complete, enemies to defeat, weapons available, etc. These details would then be converted into C++ code that generates the actual game environment and gameplay mechanics for that level.
+
+The beauty of this approach is that it allows game designers and developers to more easily create complex game environments by first designing them as a story, without having to worry about the technical implementation details upfront.
+
