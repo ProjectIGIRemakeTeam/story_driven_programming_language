b2adc3f2a22dd15ed249d5e924ac8c862c34cc52
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-07 15:56:36 +0000

    10
    plot added

--- gpt3.5/gpt3.5.js
+++ gpt3.5/gpt3.5.js
@@ -600,3 +600,70 @@ public:
 };
 You can use these C++ classes to implement the functionality of the plot_menu elements in your game.
 
+You
+
+Would you mind to create plot_elements, plot_story, plot_characters ? <story> <engine src="unreal5" platform="macos" format="c++"> <!-- This part describes the video game engine and the platform used to write the video game --> </engine> <plot type="FPS" number_of_playable_heroes="2" offline_type="coop" online_type="all"> <desc>A First Person Shooter about two espionage agents. Co-op function up to 2 players, other online game modes. Protagonist David Jones and his analyst Anya learn that a Russian Arms dealer named Josef Priboi had been kidnapped by Russian Mafia and imprisoned in a Military Airbase. After rescue Josef admits that his uncle Jach is selling a nuclear warhead to someone. After capturing the uncle, Jach Priboi admits to selling it to a Russian leader named EKK. Jones gets orders to transport Jach to headquarters with a chopper .But the chopper gets shot down by Migs near the Russian Border . EKK captures Jach and orders Jones to be killed, but Jones escapes and crosses the border and is rescued by a chopper. He hijacks the train carrying Jach, but EKK derails the train. Jones and Jach escape by a chopper sent by Anya. Jones gets orders to retrieve the warhead and terminate EKK. But he discovers a broken warhead (used to make a nuclear bomb) and fails to kill EKK as she escapes. Then he infiltrates the nuclear base where EKK planted the bomb. Jones kills EKK ,and Anya defuses the bomb. </desc> <plot_menu main="true">Start menu with new game, select map, settings </plot_menu> <plot_menu ingame="true">Character is shown in Third Person View when the game is paused. </plot_menu> <plot_element enemy="true">Enemy helicopter brings soldiers on alarm</plot_element> <plot_element playable="true">Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.</plot_element> <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story> <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story> </plot> <player_input> <input_device>Playstation 4 Controller</input_device> <input_device>Playstation 5 Controller</input_device> <input_device>Xbox 360 Controller</input_device> <input_device>Xbox One Controller</input_device> <input_device>Xbox X Controller</input_device> <input_device>Joy-Con</input_device> <input_device>Keyboard and Mouse</input_device> </player_input> </story>
+ChatGPT
+Certainly, here are the updated classes with the requested tags:
+
+Copy
+Insert
+New
+class PlotElementEnemy {
+public:
+    string description = "Enemy helicopter brings soldiers on alarm."; #plot_element,#enemy,#description
+    
+    void activate() {
+        // Activate enemy behaviors
+    }
+}
+
+class PlotElementPlayable {
+public:
+    string description = "Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with 'U.S. Army' written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission."; #plot_element,#playable,#description
+
+    void activate() {
+        // Activate playable behaviors
+    }
+}
+
+class PlotStoryAirbase {
+public:
+    string description = "Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation."; #plot_story,#airbase,#description
+}
+
+class PlotStoryTrainyard {
+public:
+    string description = "Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely."; #plot_story,#trainyard,#description
+}
+
+class PlotCharacter {
+public:
+    string name;
+    string description;
+
+    PlotCharacter(string n, string d) : name(n), description(d) {}
+
+    void talk() {
+        // Character speech logic
+    }
+}
+
+class Story {
+public:
+    string engine = "<engine src=\"unreal5\" platform=\"macos\" format=\"c++\">"; #engine,#unreal5,#macos,#story_format
+    string plotType = "FPS"; #plot_type
+    int number_of_playable_heroes = 2; #number_of_playable_heroes
+    string offline_type = "coop"; #offline_type
+    string online_type = "all"; #online_type
+    string desc = "A First Person Shooter about two espionage agents. Co-op function up to 2 players, other online game modes. Protagonist David Jones and his analyst Anya learn that a Russian Arms dealer named Josef Priboi had been kidnapped by Russian Mafia and imprisoned in a Military Airbase. After rescue Josef admits that his uncle Jach is selling a nuclear warhead to someone. After capturing the uncle, Jach Priboi admits to selling it to a Russian leader named EKK. Jones gets orders to transport Jach to headquarters with a chopper .But the chopper gets shot down by Migs near the Russian Border . EKK captures Jach and orders Jones to be killed, but Jones escapes and crosses the border and is rescued by a chopper. He hijacks the train carrying Jach, but EKK derails the train. Jones and Jach escape by a chopper sent by Anya. Jones gets orders to retrieve the warhead and terminate EKK. But he discovers a broken warhead (used to make a nuclear bomb) and fails to kill EKK as she escapes. Then he infiltrates the nuclear base where EKK planted the bomb. Jones kills EKK, and Anya defuses the bomb." #story_description
+    PlotElementEnemy enemyHelicopter; #plot_element,#enemy
+    PlotElementPlayable playableHelicopter; #plot_element,#playable
+    PlotStoryAirbase storyAirbase; #plot_story,#airbase
+    PlotStoryTrainyard storyTrainyard; #plot_story,#trainyard
+    PlotCharacter protagonist{"David Jones", "Protagonist of the game"}; #protagonist,#David_Jones
+    PlotCharacter analyst{"Anya", "Analyst who accompanies the protagonist"}; #analyst,#Anya
+};
+Let me know if you need further assistance with anything else.
+
+
