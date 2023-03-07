e33480fe9d46617afe956c7e22c321dfd7abe1a9
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-07 17:45:11 +0000

    14
    reorganise folder

--- ProjectIGI.js
+++ ProjectIGI.js
@@ -9,9 +9,16 @@
         <plot_menu main="true">Start menu with new game, select map, settings</plot_menu>
         <plot_menu ingame="true">Character is shown in Third Person View when the game is paused.</plot_menu>
         <plot_element enemy="true">Enemy helicopter brings soldiers on alarm</plot_element>
+        <plot_element>Dynamic weather on all mission/plot_story</plot_element>
         <plot_element playable="true">Our hero finally reached the helicopter. It was a Black Hawk helicopter, painted in black with "U.S. Army" written in bold yellow letters on its side. As he got inside, the roar of the engines intensified and filled up the surroundings. Our hero took control of the chopper, and they took off towards their next mission.</plot_element>
-        <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.</plot_story>
-        <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.</plot_story>
+        <plot_story>Our hero has a mission on an airbase. He needs to infiltrate the base and hack into the mainframe to gather important intel on the enemy's operation.
+            <weapon>KRISS Vector</weapon>
+            <weapon>Desert Eagle</weapon>
+        </plot_story>
+        <plot_story>Our hero has a mission on a trainyard. He needs to secure a shipment of weapons before the enemy does and then extract safely.
+            <weapon>AK-47</weapon>
+            <weapon>RPG-7</weapon>
+        </plot_story>
         <plot_character name="David Llewellyn Jones" occupation="I.G.I. agent, former British Special Air Service operator" role="Protagonist" />
         <plot_character name="Anya" occupation="Contact and mission director at headquarters" role="Mission director" />
         <plot_character name="Jach Priboi" occupation="Soviet arms dealer" role="Antagonist" />
