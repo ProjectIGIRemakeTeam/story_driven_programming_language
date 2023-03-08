7bf91830f8ef0a0d6dd7d61643fe768e7fd80ec6
ProjectIGIRemakeTeam <igiteam.remake@gmail.com>
2023-03-08 01:01:25 +0000

    27
    plot_string.json

--- react/story-editor-1.0/src/StoryEditor.js
+++ react/story-editor-1.0/src/StoryEditor.js
@@ -2,12 +2,11 @@ import  {useEffect, useRef, useState} from "react";
 
 import "./StoryEditor.css";
 
-
+import plot_strong from "./plot_string.json";
 
 function StoryEditor() {
 
-    const [story, set_story] = useState(
-    );
+    const [story, set_story] = useState(plot_strong);
 
     function didChangeStoryText(story_text) {
     set_story(story_text)
